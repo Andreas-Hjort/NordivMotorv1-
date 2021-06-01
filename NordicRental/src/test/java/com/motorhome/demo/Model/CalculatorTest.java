c84c253ab17ec035158aa3e6c4569850f67d5a9a
Andreas-Hjort <73465555+Andreas-Hjort@users.noreply.github.com>
2021-06-01 18:06:41 +0200

    Alt kode er virker

--- NordicRental/src/test/java/com/motorhome/demo/Model/CalculatorTest.java
+++ NordicRental/src/test/java/com/motorhome/demo/Model/CalculatorTest.java
@@ -14,30 +14,41 @@ class CalculatorTest {
         contracts = new Contracts();
         Cars cars = new Cars();
         Ekstras ekstras = new Ekstras();
+        Dropoff dropoff = new Dropoff();
 
+        dropoff.setDistance_in_kilometer(200);
         cars.setPrice_cars(20);
         ekstras.setPrice_extras(20);
+
+        contracts.setCars(cars);
+        contracts.setEkstras(ekstras);
+        contracts.setDropoff(dropoff);
+
         contracts.setEnd_kilometer(8000); //2200 miles driven allowed miles 2000
-        contracts.setOdometer(5800);
+        contracts.setOdometer(5980);
         contracts.setFuel(true);
-        contracts.setDate_of_handIn("2020/06/17"); //five days
-        contracts.setDate_of_Reserve("2020/06/12");
-        contracts.setDistance_in_kilometer(200);
+        contracts.setDate_of_handIn("2020-05-17"); //five days
+        contracts.setDate_of_Reserve("2020-05-12");
+        contracts.setTotal_price(500);
+
+
     }
 
     @Test
     void rentalTotalPricing() {
+        double expectedprice = 230;
+
+        double actualPrice = Calculator.rentalTotalPricing(contracts);
+
+        assertEquals(expectedprice, actualPrice);
+
     }
 
     @Test
     void contractEndingPrice() {
+        double expectedprice = 1984;
+        double actualPrice = Calculator.contractEndingPrice(contracts);
+        assertEquals(expectedprice, actualPrice);
     }
 
-    @Test
-    void contractCancelationFee() {
-    }
-
-    @Test
-    void calculateSeason() {
-    }
 }
